5f676fba94c2933ba137830dd6dda53ddb201b36
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-09 20:09:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,4 +19,8 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[2]
     win_index_3 = win_combination[3]
-  
+  end
+
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
