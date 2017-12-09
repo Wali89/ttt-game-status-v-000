cb3e3c1574b3a352a5bf68250db3f036f09b2ffa
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-09 19:51:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,14 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+WIN_COMBINATIONS = [
+  [0, 1, 2] #top
+  [3, 4, 5] #mid
+  [6, 7, 8] #bot
+  [0, 3, 6] #left
+  [1, 4, 7] #mid
+  [2, 5, 8] #right
+  [0, 4, 8]
+  [2, 4, 6]
+]
 # Define your WIN_COMBINATIONS constant
